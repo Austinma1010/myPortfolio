abf66fff56798352a3f0d4cd0c6280a64417835e
Austinma10 <austinma2168@gmail.com>
2024-04-15 21:50:51 -0400

    debugging

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import NavContainer from "./components/NavContainer";
-
+import './components/style.css';
+import 'bulma/css/bulma.min.css';
 const App = () => <NavContainer />;
 
 export default App;
