1c8fa87210b3879c0a7bb2b60074639d3d2cc624
Austinma10 <austinma2168@gmail.com>
2024-05-02 17:28:13 -0400

    more changes

--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -1,37 +1,71 @@
-import weatherImg from '../../assets/images/weatherDashboard.png'
+import weatherImg from '../../assets/images/weatherDashboard.png';
+import movieImg from '../../assets/images/final-screenshot.png';
+import dayPlanner from '../../assets/images/schedule.png';
+import shapeShift from '../../assets/images/shapeshift_ex1.png';
+
 
 export default function Blog() {
     return (
         <div className="work card"> 
 
             <h2 className="title is-3 pageHeader">My Work</h2>
-            <summary>
-                <div> 
+            <summary className="projectContainer">
+                <div className="project"> 
                     <h2 className="title is-5">Weather Dashboard</h2>
                     <a href="https://austinma1010.github.io/WeatherChecker/">
-                    <figure className="image is-128x128">
+                    <figure className="img">
                     <img src={weatherImg}></img>
                     </figure>
                     </a>
                 </div>
 
-                <div>
+                <div className="project">
+                    <h2 className="title is-5">Movie Search</h2>
+                    <a href="https://austinma1010.github.io/MovieSearchEngine/">
+                    <figure className="img">
+                    <img src={movieImg}></img>
+                    </figure>
+                    </a>
+                </div>
+
+                <div className="project">
                     <h2 className="title is-5">Day Planner</h2>
-                    <a href="https://austinma1010.github.io/Module5-Challenge/">
-                    <figure className="image is-128x128">
+                    <a>
+                    <figure className="img">
+                    <img src={dayPlanner}></img>
+                    </figure>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <h2 className="title is-5">ShapeShift</h2>
+                    <a href='https://shape-shift2-dd464e575484.herokuapp.com/login'>
+                    <figure className="img">
+                    <img src={shapeShift}></img>
+                    </figure>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <h2 className="title is-5">Movie Search</h2>
+                    <a>
+                    <figure className="img">
                     <img src={weatherImg}></img>
                     </figure>
                     </a>
                 </div>
 
-                <div>
-                    <h2 className="title is-5">PlaceHolder</h2>
+                <div className="project">
+                    <h2 className="title is-5">Movie Search</h2>
                     <a>
-                    <figure className="image is-128x128">
+                    <figure className="img">
                     <img src={weatherImg}></img>
                     </figure>
                     </a>
                 </div>
+
+             
+
             </summary>
 
         </div>
